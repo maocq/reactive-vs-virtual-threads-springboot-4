8fd252dccfd4e35b66a6ff2349b32ae5453ea42f
maocq <johncarm@bancolombia.com.co>
2025-10-19 10:33:09 -0500

    Jmeter

---
+++ test/performance-analyzer/case-two-latency-100.exs
@@ -0,0 +1,21 @@
+import Config
+
+config :distributed_performance_analyzer,
+  url: "http://_IP_:8080/api/case-two?latency=100",
+  request: %{
+    method: "GET",
+    headers: [],
+    body: ""
+  },
+  execution: %{
+    steps: 20,
+    increment: 10,
+    duration: 2000,
+    constant_load: false,
+    dataset: :none,
+    separator: ","
+  },
+  distributed: :none
+
+config :logger,
+  level: :warn
